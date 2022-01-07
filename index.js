3ffccae7ca3638b0f8c3005a76bfc76d0c5706b9
Brian Harianja <standinshd@Daniels-MacBook-Air.local>
2022-01-07 21:04:00 +0700

    Done

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
+// function anagram(a, b) {
+//     if (a.length !== b.length) return false;
+
+//     let str1 = a.split("").sort().join("");
+//     let str2 = b.split("").sort().join("");
+
+//     if (str1 === str2){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(anagram("people", "leepop"));
+function bubbleSort(array) {
+  let done = false;
+  while (!done) {
+    done = true;
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        let tmp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = tmp;
+      }
+    }
+  }
+
+  return array;
+}
+let str1 = []
+let str2 = []
 function anagram(a, b) {
-    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+        str1.push(a[i]);
+    }
 
-    let str1 = a.split("").sort().join("");
-    let str2 = b.split("").sort().join("");
+    for (let i = 0; i < b.length; i++) {
+        str2.push(b[i]);
+    }
+}
 
-    if (str1 === str2){
-        return true;
+bubbleSort(str1);
+bubbleSort(str2);
+let final1 = "";
+let final2 = "";
+
+function final(str1, str2) {
+    final1 += str1;
+    final2 += str2;
+    if (final1 === final2) {
+        return console.log(true)
     } else {
         return false
     }
 }
 
-console.log(anagram("people", "leepop"));
+console.log(anagram("abc", "cba"))
